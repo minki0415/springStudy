80921b16e3b90f66fbde47839c6d33d74b945f0f
Minki Kim <minki0415@naver.com>
2022-03-17 11:13:04 +0900

    Add files via upload

---
+++ 01. basic/bankstatement5/src/main/java/dev/bank/bankstatement/BankStatementFactory.java
@@ -0,0 +1,22 @@
+package dev.bank.bankstatement;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import dev.bank.bankstatement.data.BankStatementCSVParser;
+
+
+@Configuration // 이 클래스가 빈 구성 정보로 활용될 수 있도록 지정
+public class BankStatementFactory {
+
+    @Bean
+    public BankStatementCSVParser bankStatementCSVParser() {
+        return new BankStatementCSVParser();
+    }
+
+    @Bean // 빈 구성 정보 내 등록되어 있는 빈
+    public BankStatementAnalyzer bankStatementAnalyzer() {
+        BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer(bankStatementCSVParser());
+        return bankStatementAnalyzer;
+    }
+}
